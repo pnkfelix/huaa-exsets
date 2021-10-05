11e8bc89d15682db0bc8107fb34c1cb89e31236d
Felix Klock <pnkfelix@amazon.com>
2021-10-05 10:20:36 -0400

    different end state for ex02.

--- ex02/src/main.rs
+++ ex02/src/main.rs
@@ -1,15 +1,20 @@
 #[tokio::main]
 async fn main() {
+    let t = try_it();
     println!("Hello, world!");
-
-    assert_eq!(double(4).await, 8);
+    t.await;
 }
 
-async fn double(x: u32) -> u32 {
-    x * 2
+async fn add(x: i32, y: i32) -> i32 {
+    println!("adding x: {} and y: {}", x, y);
+    x + y
 }
 
-#[tokio::test]
-async fn double_four() {
-    assert_eq!(double(4).await, 8);
+async fn try_it() {
+    let nums = vec![1, 10, 100];
+    let mut sums = Vec::with_capacity(nums.len());
+    for s in nums.into_iter().map(|n| add(n, 2)) {
+       sums.push(s.await);
+    }
+    println!("answer: {:?}", sums);
 }
