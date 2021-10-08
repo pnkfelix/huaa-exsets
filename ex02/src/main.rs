70ff72af29277526db76c41e55c7e5308fcf988a
Felix Klock <pnkfelix@amazon.com>
2021-10-08 10:39:15 -0400

    done with ex02.

--- ex02/src/main.rs
+++ ex02/src/main.rs
@@ -1,4 +1,30 @@
 #[tokio::main]
 async fn main() {
+    let t = try_it();
     println!("Hello, world!");
+    t.await;
 }
+
+async fn add(x: i32, y: i32) -> i32 {
+    println!("adding x: {} and y: {}", x, y);
+    x + y
+}
+
+async fn try_it() {
+    let answer = add(1, 2);
+    println!("answer: {:?}", answer.await);
+}
+
+/*
+fn try_sums_sync() {
+    let nums = vec![1, 10, 100];
+    let sums = nums.into_iter().map(|n| n + 2).collect();
+    println!("answer: {:?}", sums);
+}
+
+async fn try_sums() {
+    let nums = vec![1, 10, 100];
+    let sums = nums.into_iter().map(|n| add(n, 2).await).collect();
+    println!("answer: {:?}", sums);
+}
+*/
