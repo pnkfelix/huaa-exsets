1bb00c9225df70fc8767fc281c5616a2a70d2247
Felix Klock <pnkfelix@amazon.com>
2021-10-08 14:00:24 -0400

    initial state for ex08.

---
+++ ex08/src/main.rs
@@ -0,0 +1,60 @@
+const APACHE: &'static str = "https://www.apache.org/";
+const AMAZON: &'static str = "https://www.amazon.com/";
+const DOCS_RS: &'static str = "https://docs.rs/";
+const MOZILLA: &'static str = "https://www.mozilla.org/";
+const RUST_LANG: &'static str = "https://www.rust-lang.org/";
+const WIKIPEDIA: &'static str = "http://www.wikipedia.org/";
+
+type MyError = Box<dyn std::error::Error + Send + Sync>;
+
+#[tokio::main]
+async fn main() -> Result<(), MyError> {
+    use tokio::task::JoinHandle;
+
+    let mut site_handles: Vec<(&'static str, JoinHandle<_>)> = Vec::new();
+
+    let mut push_site = |site| {
+        site_handles.push((site, tokio::task::spawn(first_url(site))));
+    };
+
+    for site in [APACHE, AMAZON, DOCS_RS, MOZILLA, RUST_LANG, WIKIPEDIA] {
+        push_site(site);
+    }
+
+    for (site, handle) in site_handles {
+        dbg!((site, handle.await??));
+    }
+
+    Ok(())
+}
+
+async fn first_url(site: &str) -> Result<Option<Url>, MyError>
+{
+    let response = reqwest::get(site).await?;
+    let text = response.text().await?;
+    // println!("response text: {} bytes", text.len());
+    first_url_in_text(&text)
+}
+
+use url::Url;
+use scraper::{Html, Selector};
+
+fn first_url_in_text(text: &str) -> Result<Option<Url>, MyError>
+{
+    let doc = Html::parse_document(&text);
+    // (This unwrap should never fail; the input is a known constant.)
+    let selector = Selector::parse("a")
+        .unwrap_or_else(|err| panic!("failed to parse tag `a`: {:?}.", err));
+    for element in doc.select(&selector) {
+        let link = match element.value().attr("href") {
+            Some(link) => link,
+            None => continue,
+        };
+        let url = match Url::parse(link) {
+            Ok(u) => u,
+            Err(_) => continue,
+        };
+        return Ok(Some(url));
+    }
+    return Ok(None);
+}
