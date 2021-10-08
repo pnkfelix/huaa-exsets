a81bb118ac0671f83d4a8c482b333cbaa1e01a9e
Felix Klock <pnkfelix@amazon.com>
2021-10-08 10:44:01 -0400

    done with ex 3.0.

--- ex03/src/main.rs
+++ ex03/src/main.rs
@@ -1,4 +1,7 @@
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::get("http://www.wikipedia.org/").await?;
+    println!("response text: {} bytes", response.text().await?.len());
+    Ok(())
 }
+
