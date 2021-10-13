5eba016ac33b9b5a30d677c6708b2ec89b7800f7
Felix Klock <pnkfelix@amazon.com>
2021-10-13 17:44:18 -0400

    ex04.1-done

--- toys/src/main.rs
+++ toys/src/main.rs
@@ -1,3 +1,17 @@
+// Computes factorial of n, printing progress and yielding along the way.
+async fn fact_yielding(n: u32) -> f64 {
+    let mut i = 0_u32;
+    let mut accum = 1_f64;
+    loop {
+        println!("i: {I} fact_{N}({I}): {A}", N=n, I=i, A=accum);
+        if i == n { break; }
+        tokio::task::yield_now().await;
+        i += 1;
+        accum *= i as f64;
+    }
+    return accum;
+}
+
 // Computes factorial of n, printing progress along the way.
 async fn fact_stiff(n: u32) -> f64 {
     let mut i = 0_u32;
@@ -15,7 +29,7 @@ async fn fact_stiff(n: u32) -> f64 {
 async fn main() {
     let answer = tokio::select! {
         fact_5 = fact_stiff(5) => fact_5,
-        fact_10 = fact_stiff(10) => fact_10,
+        fact_10 = fact_yielding(10) => fact_10,
     };
     println!("answer: {}", answer);
 }
