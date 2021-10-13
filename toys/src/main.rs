c8e5e3b9a19c58a524a9e73b2cd9ca8deb20cdee
Felix Klock <pnkfelix@amazon.com>
2021-10-13 17:09:54 -0400

    ex02.3-done

--- toys/src/main.rs
+++ toys/src/main.rs
@@ -14,3 +14,17 @@ async fn try_it() {
     let answer = add(1, 2).await;
     println!("answer: {:?}", answer);
 }
+
+/*
+fn try_sums_sync() {
+    let nums = vec![1, 10, 100];
+    let sums: Vec<i32> = nums.into_iter().map(|n| n + 2).collect();
+    println!("answer: {:?}", sums);
+}
+
+async fn try_sums() {
+    let nums = vec![1, 10, 100];
+    let sums: Vec<i32> = nums.into_iter().map(|n| add(n, 2).await).collect();
+    println!("answer: {:?}", sums);
+}
+*/
