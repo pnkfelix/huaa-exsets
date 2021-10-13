2fab36df72c5ddfad87de0d207d6c738d0789b9c
Felix Klock <pnkfelix@amazon.com>
2021-10-13 18:03:59 -0400

    ex06.1-done

--- toys/src/main.rs
+++ toys/src/main.rs
@@ -29,6 +29,6 @@ async fn fact_stiff(n: u32) -> f64 {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let fact_5 = tokio::task::spawn(fact_stiff(5));
     let fact_10 = tokio::task::spawn(fact_yielding(10));
-    dbg!((fact_5.await?, fact_10.await?));
+    dbg!((fact_5.await?, fact_10));
     Ok(())
 }
