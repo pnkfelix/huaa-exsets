e930a946da96dc9e7b53fc4a13b185cdb8079128
Felix Klock <pnkfelix@amazon.com>
2021-10-13 17:26:41 -0400

    ex03.1-done

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,6 +1,35 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let response = reqwest::get("https://www.wikipedia.org/").await?;
-    println!("response text: {} bytes", response.text().await?.len());
+    let wikipedia_url = first_url("https://www.wikipedia.org/").await?;
+    dbg!(wikipedia_url);
     Ok(())
+}
+
+use url::Url;
+use scraper::{Html, Selector};
+
+async fn first_url(site: &str) -> Result<Option<Url>, Box<dyn std::error::Error>> {
+    let response = reqwest::get(site).await?;
+    let text = response.text().await?;
+    first_url_in_text(&text)
+}
+
+fn first_url_in_text(text: &str) -> Result<Option<Url>, Box<dyn std::error::Error>>
+{
+    let doc = Html::parse_document(&text);
+    // (This unwrap should never fail; the input is a known constant.)
+    let selector = Selector::parse("a")
+        .unwrap_or_else(|err| panic!("failed to parse tag `a`: {:?}.", err));
+    for element in doc.select(&selector) {
+        let link = match element.value().attr("href") {
+            Some(link) => link,
+            None => continue,
+        };
+        let url = match Url::parse(link) {
+            Ok(u) => u,
+            Err(_) => continue,
+        };
+        return Ok(Some(url));
+    }
+    return Ok(None);
 }
