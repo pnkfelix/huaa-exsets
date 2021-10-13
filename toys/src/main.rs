9a0189b692bdd42bc812b243f811b9bc573e5d8d
Felix Klock <pnkfelix@amazon.com>
2021-10-13 17:33:46 -0400

    ex04.0-done

--- toys/src/main.rs
+++ toys/src/main.rs
@@ -1,30 +1,21 @@
+// Computes factorial of n, printing progress along the way.
+async fn fact_stiff(n: u32) -> f64 {
+    let mut i = 0_u32;
+    let mut accum = 1_f64;
+    loop {
+        println!("i: {I} fact_{N}({I}): {A}", N=n, I=i, A=accum);
+        if i == n { break; }
+        i += 1;
+        accum *= i as f64;
+    }
+    return accum;
+}
+
 #[tokio::main]
 async fn main() {
-    let t = try_it();
-    println!("Hello, world!");
-    t.await;
+    let answer = tokio::select! {
+        fact_5 = fact_stiff(5) => fact_5,
+        fact_10 = fact_stiff(10) => fact_10,
+    };
+    println!("answer: {}", answer);
 }
-
-async fn add(x: i32, y: i32) -> i32 {
-    println!("adding x: {} and y: {}", x, y);
-    x + y
-}
-
-async fn try_it() {
-    let answer = add(1, 2).await;
-    println!("answer: {:?}", answer);
-}
-
-/*
-fn try_sums_sync() {
-    let nums = vec![1, 10, 100];
-    let sums: Vec<i32> = nums.into_iter().map(|n| n + 2).collect();
-    println!("answer: {:?}", sums);
-}
-
-async fn try_sums() {
-    let nums = vec![1, 10, 100];
-    let sums: Vec<i32> = nums.into_iter().map(|n| add(n, 2).await).collect();
-    println!("answer: {:?}", sums);
-}
-*/
