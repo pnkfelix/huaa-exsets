f9d026f2ecc0b31de0729246f81d2dd4ef914691
Felix Klock <pnkfelix@amazon.com>
2021-10-13 17:04:25 -0400

    ex02.0-done

--- toys/src/main.rs
+++ toys/src/main.rs
@@ -2,3 +2,14 @@
 async fn main() {
     println!("Hello, world!");
 }
+
+async fn add(x: i32, y: i32) -> i32 {
+    println!("adding x: {} and y: {}", x, y);
+    x + y
+}
+
+#[cfg(does_not_compile)]
+fn try_it() {
+    let answer = add(1, 2);
+    println!("answer: {:?}", answer);
+}
