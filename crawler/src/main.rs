8a27c3b3b13ab311c88fb547b933de273fd4386f
Felix Klock <pnkfelix@amazon.com>
2021-10-13 21:14:34 -0400

    ex08.0-done

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -6,15 +6,27 @@ const WIKIPEDIA: &'static str = "http://www.wikipedia.org/";
 
 type MyError = Box<dyn std::error::Error + Send + Sync>;
 
+use tokio::sync::mpsc::{channel, Sender};
+
+#[derive(Debug)]
+struct Msg {
+    site: Url,
+    link: Url,
+}
+
+const MSG_BUF_SIZE: usize = 4;
+
 #[tokio::main]
 async fn main() -> Result<(), MyError> {
     use tokio::task::JoinHandle;
+
+    let (tx, rx) = channel::<Msg>(MSG_BUF_SIZE);
 
     let mut site_handles: Vec<(&'static str, JoinHandle<_>)> = Vec::new();
 
     let sites = [AMAZON, DOCS_RS, MOZILLA, RUST_LANG, WIKIPEDIA];
     for site in sites {
-        site_handles.push((site, tokio::task::spawn(first_url(site))));
+        site_handles.push((site, tokio::task::spawn(all_urls(site, tx.clone()))));
     }
 
     for (site, handle) in site_handles {
@@ -32,14 +44,22 @@ async fn _always_err() -> Result<(), MyError> {
 use url::Url;
 use scraper::{Html, Selector};
 
-async fn first_url(site: &str) -> Result<Option<Url>, MyError> {
+async fn all_urls(site: &str, tx: Sender<Msg>) -> Result<usize, MyError> {
     let response = reqwest::get(site).await?;
+    let site = Url::parse(site)?;
     let text = response.text().await?;
-    first_url_in_text(&text)
+    // println!("response text: {} bytes", text.len());
+    let urls = all_urls_in_text(&text)?;
+    let count = urls.len();
+    for url in urls {
+        tx.send(Msg { site: site.clone(), link: url }).await?
+    }
+    Ok(count)
 }
 
-fn first_url_in_text(text: &str) -> Result<Option<Url>, MyError>
+fn all_urls_in_text(text: &str) -> Result<Vec<Url>, MyError>
 {
+    let mut discovered = Vec::new();
     let doc = Html::parse_document(&text);
     // (This unwrap should never fail; the input is a known constant.)
     let selector = Selector::parse("a")
@@ -53,7 +73,7 @@ fn first_url_in_text(text: &str) -> Result<Option<Url>, MyError>
             Ok(u) => u,
             Err(_) => continue,
         };
-        return Ok(Some(url));
+        discovered.push(url);
     }
-    return Ok(None);
+    Ok(discovered)
 }
