01c384ab4b7c922ad8047c206a071d9a131419e3
Felix S. Klock II <pnkfelix@pnkfx.org>
2021-11-04 14:39:05 -0400

    checkpoint state with console integrated to show to others.

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -16,10 +16,11 @@ struct Msg {
 
 const MSG_BUF_SIZE: usize = 4;
 
-const MAX_DEPTH: usize = 2;
+const MAX_DEPTH: usize = 3;
 
 #[tokio::main]
 async fn main() -> Result<(), MyError> {
+    console_subscriber::init();
     let mut sites: Vec<Url> = Vec::new();
     for link in [AMAZON, DOCS_RS, MOZILLA, RUST_LANG, WIKIPEDIA] {
         sites.push(Url::parse(link)?);
